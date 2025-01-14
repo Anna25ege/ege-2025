96c0f0a588c983b9f7dc8bb7345bf7b60b23156e
AMelnikova <byssa169@gmail.com>
2025-01-14 19:51:18 +0200

    https://t.me/c/2147455670/25

---
+++ 13-01-25/homework.py
@@ -0,0 +1,41 @@
+# 1
+students ='Anna, Sofia, Maria'
+print(students)
+
+# 2
+num= 17
+print(num)
+
+# 3
+float_num=3.14
+print(float_num)
+
+#4
+print(id(num))
+print(type(float_num))
+
+#5
+print(num + float_num)
+print (type(num + float_num))
+
+#6
+my_list =[1, 3, 'text', (5, 7, 9), 2.18]
+print(my_list)
+
+#7
+my_dict = {"Имя":"Иван",
+           "Фамилия":"Иванов",
+           "Возраст":17,
+           "Класс":"11А"}
+print(my_dict)
+
+#8
+my_str='175'
+float=15.5
+print(int(my_str) + float)
+
+#9
+num_1 = input('Введите имя:')
+welcome_text= 'Привет,'
+print(welcome_text+ num_1)
+
