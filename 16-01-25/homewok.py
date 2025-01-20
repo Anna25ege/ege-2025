c8abdf8efe67acc18d1d283e2fb7329b2bb7548e
AMelnikova <byssa169@gmail.com>
2025-01-20 17:28:26 +0200

    №2 не сделан. разложение на множители

---
+++ 16-01-25/homewok.py
@@ -0,0 +1,21 @@
+#1
+num1 = int(input('Введите натуральное число:'))
+num2 = int(input('Введите натуральное число:'))
+
+print(num1 + num2)
+print(num1 - num2)
+print(num1 * num2)
+print(num1 / num2)
+print(num1 // num2)
+print(num1 % num2)
+
+#2
+num1 = int(input('Введите пятизначное число:'))
+
+#3
+h = int(input('Введите высоту:'))
+r = int(input('Введите радиус:'))
+π=3.14
+S1=2*π*r*h
+S2=π*r*2
+print(S1,S2)
