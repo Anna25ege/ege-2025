3aca576f9a2c2a6763f3cd5ba34342248a048768
AMelnikova <byssa169@gmail.com>
2025-01-23 19:49:13 +0200

    условный оператор

---
+++ 23-01-25/lesson-5.3.py
@@ -0,0 +1,19 @@
+num1 = 5
+num2 = 30
+
+f1 = num1 % 2 == 0 # false
+f2 = num2 % 10 == 0 # true
+f3 = 10 <= num1+num2 <= 99 # true
+
+if f1 and f2:
+    print('text1')
+
+if f1 or f2
+    print('text2')
+
+if not f1 and f2:
+    print('text3')
+
+if (f2 == f3) and f1:
+    print("text4")
+
---
+++ 23-01-25/lesson-5.py
@@ -0,0 +1,9 @@
+# тип данных -> типы, в которые можно преобразовать
+var_int = 5 # -> float() , str(), bool()
+var_dict = {'name': 'Anna', 'age': 18} # -> list(), tuple(), bool(), set()
+var_set = {5, 25, 40,5} # -> list(), tuple(), bool()
+var_str = '67' #->int(), float(), list(), tuple(), set(), bool()
+var_list = [678, 7.9, 5, 'hell'] # -> tuple(), bool(), set()
+var_float = 48.8 # -> int(), str(), bool()
+var_tuple = (4, 7.9, '5') # -> list(), set(), bool()
+var_bool= True
