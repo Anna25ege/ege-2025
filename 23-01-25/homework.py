c3088fcc0de903d0f38c9868ab60fdd1f9aeecbc
AMelnikova <byssa169@gmail.com>
2025-01-27 16:39:25 +0200

    дз. что такое псевдослучайное число?

---
+++ 23-01-25/homework.py
@@ -0,0 +1,35 @@
+#1
+num = int(input())
+if num % 10 == 0:
+    print(num)
+elif num % 10 == 5:
+    print(num)
+elif num % 10 == 2:
+    print (num)
+else:
+    print(num+1)
+#2
+num = int(input('Введите число:'))
+if num> 0:
+    print(num+1)
+elif num<0:
+    print(num-1)
+#3
+
+#4
+
+x = int(input('Введите число:'))
+if x>0:
+    y =2*x+5
+elif x==0:
+    y=x
+else:
+    y=2*x-1
+print('y =',y)
+
+#5
+
+
+
+
+
