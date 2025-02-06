edfab4122afa0e59a64495238685acae0f3884c8
AMelnikova <byssa169@gmail.com>
2025-02-06 19:57:03 +0200

    https://kompege.ru/variant?kim=25065613 пример

---
+++ 06-02-25/task-6-9775.py
@@ -0,0 +1,29 @@
+from turtle import *
+
+tracer(0)
+lt(90)
+m = 10
+
+for i in range (2):
+    fd(13*m)
+    rt(90)
+    fd(20*m)
+    rt(90)
+up()
+fd(8*m)
+rt(90)
+bk(3*m)
+lt(90)
+down()
+for i in range(2):
+    fd(16*m)
+    rt(90)
+    fd(8*m)
+    rt(90)
+up()
+for x in range(-20, 30):
+    for y in range(-20, 30):
+        goto(x*m,y*m)
+        dot(3,'red')
+update()
+done()
