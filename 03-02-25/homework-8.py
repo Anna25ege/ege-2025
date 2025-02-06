4e5028bc2ff30bae7a00ad7cc17397b5a206a226
AMelnikova <byssa169@gmail.com>
2025-02-06 16:27:48 +0200

    домашка

---
+++ 03-02-25/homework-8.py
@@ -0,0 +1,26 @@
+#1
+words = input('Введите слова:')
+words = words.split()
+
+for word in words:
+    if word == word[::-1]:
+        print(word)
+
+#2
+words = 'qdwrsatdwuvwxyazabcddweoklfghijaklmpadbcdwefakg'
+ #1-
+ #2
+print(words[::3])
+
+ #3-
+
+ #4
+print(len(words[2:7]))
+ #5
+print(words[::-1][2:14])
+
+#3-
+
+string = ' abcd defg ghij
+#4-
+#5-
