1d21aceb6bbb5647df1106ff4723e53d23ebc86d
AMelnikova <byssa169@gmail.com>
2025-02-17 19:55:57 +0200

    https://kompege.ru/variant?kim=25067201

---
+++ 17-02-25/task-5-1849.py
@@ -0,0 +1,12 @@
+ans = []
+for n in range(1,10000):
+    R = bin(n)[2::]
+    if n%2 == 0:
+        R = '1' + R + '0'
+    else:
+        R = '11' + R + '11'
+    R = int(R,2)
+    if R > 52:
+        ans.append(R)
+
+print(min(ans))
