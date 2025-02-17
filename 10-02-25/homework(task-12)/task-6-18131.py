c58e749eb1b04f4c07f737315222dab067599186
AMelnikova <byssa169@gmail.com>
2025-02-17 16:25:23 +0200

    https://kompege.ru/variant?kim=25066246

---
+++ 10-02-25/homework(task-12)/task-6-18131.py
@@ -0,0 +1,33 @@
+from turtle import *
+tracer(0)
+screensize(1000,1000)
+pensize(3)
+m =10
+lt(90)
+
+color('pink')
+for i in range(9):
+    fd(22*m)
+    rt(90)
+    fd(6*m)
+    rt(90)
+up()
+fd(1*m)
+rt(90)
+fd(5*m)
+lt(90)
+down()
+color('blue')
+for i in range(9):
+    fd(53*m)
+    rt(90)
+    fd(75*m)
+    rt(90)
+up()
+for x in range(-40,40):
+    for y in range(-40,40):
+        goto(x*m,y*m)
+        dot(3,'black')
+
+update()
+done()
