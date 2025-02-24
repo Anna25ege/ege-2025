be1241d01d43cc60412fcb795353357d7a51bff5
AMelnikova <byssa169@gmail.com>
2025-02-24 14:47:52 +0200

    renamed

--- 17-02-25/task-12-9774.py
+++
@@ -1,9 +0,0 @@
-ans = []
-for n in range(1,10000):
-    def convert(num, sys):
-        res = ''
-        while num:
-            res += str(num % sys)
-            num //= sys
-        return res[::-1]
-    if res.count('1')% 3 == 0:
