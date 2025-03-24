af468afa28b6b9fbe208de072995927bf5a7474e
AMelnikova <byssa169@gmail.com>
2025-03-25 00:06:13 +0200

    https://kompege.ru/variant?kim=25073519

---
+++ 17-03-25/task-8-18974.py
@@ -0,0 +1,25 @@
+from string import digits, ascii_uppercase
+from itertools import product
+
+alph = digits + ascii_uppercase
+cnt = 0
+for i in product(alph[:25],repeat=4):
+    i = ''.join(i)
+    if i[0] != '0':
+        count_nech = 0
+        #перебираем все  енчетные цифры - от первой позиции берем каждый второй символ
+        for j in alph[1::2]:
+            count_nech += i.count(j)
+
+        count_5 = 0
+        for j in '012345':
+            count_5 += i.count(j)
+
+        if count_nech == 1 and count_5 <= 2:
+            cnt +=1
+
+print(cnt)
+
+
+
+
