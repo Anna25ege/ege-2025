286db6ec10dbb2425d43de951628c73188ef0a9b
AMelnikova <byssa169@gmail.com>
2025-04-03 19:40:10 +0200

    https://kompege.ru/variant?kim=25076534

--- 27-03-25/homework/task-2-78.py
+++ 27-03-25/homework/task-2-78.py
@@ -1,7 +1,7 @@
 from itertools import product, permutations
 
 def f(x, y, z, w):
-    return (x <= y and (not z)) or w
+    return (x <= (y and (not z))) or w
 
 for a1, a2, a3, a4, a5, a6 in product([0,1],repeat=6):
     table = [(a1, a2, 1, 0), (0, a3, a4, 1), (1, a5, 1, a6)]
