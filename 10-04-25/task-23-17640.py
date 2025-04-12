fd642359b91f6f654436b2f7eda7ba5751db78f5
AMelnikova <byssa169@gmail.com>
2025-04-12 21:01:56 +0200

    .

--- 10-04-25/task-23-17640.py
+++ 10-04-25/task-23-17640.py
@@ -1,8 +1,9 @@
-def f(cur,end):
-    if cur== end:
+def f(cur, end):
+    if cur == end:
         return 1
     if cur < end:
         return 0
-    return f(cur - 2,end) + f(cur //2,end)
+    return f(cur - 2, end) + f(cur // 2, end)
 
-print(f(32,14)* f(14,1))
+
+print(f(32, 14) * f(14, 1))
