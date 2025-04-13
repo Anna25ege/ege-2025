15e65a9fc89f9ee0f947f52691596df745762274
AMelnikova <byssa169@gmail.com>
2025-04-13 20:06:45 +0200

    https://kompege.ru/variant?kim=25077094

---
+++ 07-04-25/task-16-19248.py
@@ -0,0 +1,12 @@
+from sys import setrecursionlimit
+
+
+def f(n):
+    if n < 5:
+        return n
+    if n >= 5:
+        return 2 * n * f(n - 4)
+
+
+setrecursionlimit(100000)
+print((f(13766) - 9 * f(13762)) // f(13758))
