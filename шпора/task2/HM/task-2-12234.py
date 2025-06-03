b5e2f3ea5457faae80da5f52e2ab8608ab469a3d
AMelnikova <byssa169@gmail.com>
2025-06-03 23:10:25 +0200

    https://kompege.ru/variant?kim=25102453

---
+++ шпора/task2/HM/task-2-12234.py
@@ -0,0 +1,16 @@
+from itertools import product, permutations
+
+
+def f(x, z, w, y):
+    return (y and ((x or z)) or ((not (y or z))) or w)
+
+
+for a1, a2, a3, a4 in product([0, 1], repeat=4):
+    table = [(1, a1, 0, 1),
+             (a2, 1, 0, a3),
+             (0, 0, a4, 1)]
+    if len(table) == len(set(table)):
+        for p in permutations('wzxy'):
+            u = [f(**dict(zip(p, t))) for t in table]
+            if u == [0, 0, 0]:
+                print(*p)
