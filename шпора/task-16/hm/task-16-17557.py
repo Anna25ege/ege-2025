b627201301bc7fdd274d725749b97efcf53dfeab
AMelnikova <byssa169@gmail.com>
2025-06-08 18:20:07 +0200

    https://kompege.ru/variant?kim=25103245

---
+++ шпора/task-16/hm/task-16-17557.py
@@ -0,0 +1,12 @@
+from sys import setrecursionlimit
+
+
+def f(n):
+    if n == 1:
+        return 1
+    if n > 1:
+        return 2 * n * f(n - 1)
+
+setrecursionlimit(3000)
+
+print((f(2024)//16-f(2023))//f(2022))
